90a2bec69dde09632d8f77eccddd2458b94cfebd
Eric Bartels <ebartels@gmail.com>
2016-09-19 18:16:40 -0700

    Make some important directories

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 # check for stow
 type stow 2> /dev/null > /dev/null || echo 'Install "stow" to install files (e.g. apt-get install stow)'
 
+mkdir -p ~/.local ~/.config ~/.bashrc.d/
+
 # Uses stow to install symlinked files
 for dir in */; do
     stow -v --target="$HOME" "$@" "$dir"
